cf0b9728dbd66860e8930e9dbaae263a272566a9
Ibn-Adam <150707325+Ibn-Adam@users.noreply.github.com>
2024-12-09 20:04:01 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "Ibn adam",
+  age: 25,
+  gender : "male",
+  country: false
+}
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+let country = person.country;
+  
